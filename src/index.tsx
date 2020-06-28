22d55be6233c1a1e7d8f13d429e32f6e88abbc94
Charles Liu <liucharlesche@gmail.com>
2020-06-28 20:00:50 +0900

    remove react router

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './styles/index.scss';
 
-import { BrowserRouter as Router } from 'react-router-dom';
-
-ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
-    document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
