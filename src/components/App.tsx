d83e643ae40c7cc51ac1b2329503f563706ec91e
Charles Liu <liucharlesche@gmail.com>
2020-06-28 20:04:34 +0900

    add header main and loading screen into app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,47 @@
 import React from 'react';
 
+import { Movie } from '../types';
+
 import Header from './Header';
+import Main from './Main';
+import LoadingScreen from './LoadingScreen';
 
 interface State {
-    currentMovie: string;
+    currentMovie: Movie | null;
+    isLoading: boolean;
 }
 
 class App extends React.PureComponent<{}, State> {
     constructor(props: {}) {
         super(props);
         this.state = {
-            currentMovie: '',
+            currentMovie: null,
+            isLoading: false,
         };
+
+        this.setCurrentMovie = this.setCurrentMovie.bind(this);
+        this.setIsLoading = this.setIsLoading.bind(this);
     }
 
-    setCurrentMovie(movie: string): void {
+    setCurrentMovie(movie: Movie): void {
         this.setState({ currentMovie: movie });
     }
 
+    setIsLoading(isLoading: boolean): void {
+        this.setState({ isLoading });
+    }
+
     render(): JSX.Element {
-        const { currentMovie } = this.state;
+        const { currentMovie, isLoading } = this.state;
         return (
             <>
                 <Header currentMovie={currentMovie} />
+                <Main
+                    currentMovie={currentMovie}
+                    setCurrentMovie={this.setCurrentMovie}
+                    setIsLoading={this.setIsLoading}
+                />
+                <LoadingScreen isLoading={isLoading} />
             </>
         );
     }
