e30359a6c82422a8d006a6b9b508815482a4397f
Charles Liu <liucharlesche@gmail.com>
2020-06-28 20:07:37 +0900

    remove Link from react-router

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,22 +2,12 @@ import React from 'react';
 import '../styles/index.scss';
 import { Movie } from '../types';
 
-import { Link } from 'react-router-dom';
-
 interface Props {
     currentMovie: Movie | null;
 }
 
 const Header = ({ currentMovie }: Props): JSX.Element => (
-    <header>
-        {currentMovie ? (
-            <h1>{currentMovie.title}</h1>
-        ) : (
-            <Link to="/">
-                <img className="logo" src="src/assets/logo.png" />
-            </Link>
-        )}
-    </header>
+    <header>{currentMovie ? <h1>{currentMovie.title}</h1> : <img className="logo" src="src/assets/logo.png" />}</header>
 );
 
 export default Header;
